3d1c7b8527167ffae89e873ee40d004363fccd4e
Dominik Darłak <dominik.darlak@outlook.com>
2025-09-30 17:38:10 +0200

    feat(ksqldb): add KsqlInitializer for 1-minute BTC avg aggregation

--- CryptoDataProducer/src/main/java/io/crypto/realtime/producer/ksql/KsqlInitializer.java
+++ CryptoDataProducer/src/main/java/io/crypto/realtime/producer/ksql/KsqlInitializer.java
@@ -10,9 +10,8 @@ public final class KsqlInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(BtcProducer.class.getSimpleName());
 
-    public static void initBinanceAvgPerMin(String ksqlUrl,
-                                        String sourceTopic,   // np. "crypto.realtime.data.btc"
-                                        String sinkTopic) {   // np. "btc_avg_1m"
+    public static void initBinanceAvgPerMin(String ksqlUrl, String sourceTopic, String sinkTopic) {
+
         KsqlDbClient ksql = new KsqlDbClient(ksqlUrl);
 
         String script = String.join("\n",
@@ -46,5 +45,43 @@ public final class KsqlInitializer {
 
         String response = ksql.execute(script);
         log.info("ksqlDB init response: {}", response);
+    }
+
+    public static void initBinanceAvgPerMinFinal(String ksqlUrl, String sourceTopic, String sinkTopic) {
+
+        KsqlDbClient ksql = new KsqlDbClient(ksqlUrl);
+
+        String script = String.join("\n",
+                "CREATE STREAM IF NOT EXISTS BINANCE_TRADES_RAW (",
+                "  SYMBOL        VARCHAR KEY,",
+                "  EVENTTYPE     VARCHAR,",
+                "  EVENTTIME     BIGINT,",
+                "  TRADEID       BIGINT,",
+                "  TRADETIME     BIGINT,",
+                "  ISBUYERMAKER  BOOLEAN,",
+                "  PRICE         DOUBLE,",
+                "  QUANTITY      DOUBLE",
+                ") WITH (",
+                "  KAFKA_TOPIC='" + sourceTopic + "',",
+                "  VALUE_FORMAT='JSON',",
+                "  TIMESTAMP='TRADETIME'",
+                ");",
+                "",
+                "CREATE TABLE IF NOT EXISTS BTC_AVG_1M_FINAL",
+                "  WITH (KAFKA_TOPIC='" + sinkTopic + "', VALUE_FORMAT='JSON') AS",
+                "SELECT",
+                "  SYMBOL,",
+                "  TIMESTAMPTOSTRING(WINDOWSTART, 'yyyy-MM-dd HH:mm:ss', 'Europe/Warsaw') AS WINDOW_START,",
+                "  TIMESTAMPTOSTRING(WINDOWEND,   'yyyy-MM-dd HH:mm:ss', 'Europe/Warsaw') AS WINDOW_END,",
+                "  AVG(PRICE) AS AVG_PRICE",
+                "FROM BINANCE_TRADES_RAW",
+                "WINDOW TUMBLING (SIZE 1 MINUTE)",
+                "GROUP BY SYMBOL",
+                "EMIT FINAL;"
+        );
+
+        String response = ksql.execute(script);
+        log.info("ksqlDB init FINAL response: {}", response);
+
     }
 }
